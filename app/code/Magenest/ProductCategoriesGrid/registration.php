d268d78f0e3a659fa63c7244723bfbdca4bb01cc
cuongth99999 <cuongth99999@gmail.com>
2026-05-25 11:22:13 +0700

    Section7_Exam3: Categories Filter product listing grid

---
+++ app/code/Magenest/ProductCategoriesGrid/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * app/code/Magenest/ProductCategoriesGrid/registration.php
+ */
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Magenest_ProductCategoriesGrid',
+    __DIR__
+);
